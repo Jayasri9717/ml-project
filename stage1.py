49b16ae1b84db5c6c8a0ad964ff70eb5a38c8ac5
JJJRRR <ritikasri02@gmail.com>
2026-02-24 22:05:41 +0530

    Final calculator assignment

---
+++ stage1.py
@@ -0,0 +1,21 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter second number:"))
+op=input("ENTER operator(+,-,*,/):")
+
+if op=="+":
+    result=num1+num2
+elif op=="-":
+    result=num1-num2
+elif op=="*":
+    result=num1*num2
+elif op=="/":
+    if num2!=0:
+        result=num1/num2
+    else:
+         print("Division by zero is not allowed")
+         exit()
+else:
+    print("Invalid operator")
+    exit()
+
+print("Result =",result)
